2f7a3c108341daf5f99100da00dc02a58996fbd8
jkljkl1197 <jordan1197@hotmail.ca>
2017-03-14 10:24:32 -0400

    test

--- Interface_3fev/Personne.cs
+++ Interface_3fev/Personne.cs
@@ -5,74 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Interface_3fev {
+
     public class Personne {
-        public enum statusEnum {
-            Celibataire,
-            Marie,
-            Veuf,
-            Divorce
-        };
-        public enum sexeEnum {
-            Homme,
-            Femme
-        };
+    private string _nom;
+    private string _prenom;
+    private int _age;
 
-        # region Get/Set
-        public string Nas {
-            get;
-            set;
-        }
-        public string Nom {
-            get;
-            set;
-        }
-        public string Prenom {
-            get;
-            set;
-        }
-        public string DateDeNaissance {
-            get;
-            set;
-        }
-        public double ? Depense {
-            get;
-            set;
-        }
-        public statusEnum ? Status {
-            get;
-            set;
-        }
-        public sexeEnum ? Sexe {
-            get;
-            set;
-        }
-        # endregion
+    public Personne() : this("Skywalker", "Anakin", 25) { }
 
-        # region Fonction / Constructeur
-        public Personne(string nom, string prenom, double ? depense) {
-            this.Nom = nom;
-            this.Prenom = prenom;
-            this.Depense = depense;
-        }
-
-        public Personne(string nas, string nom, string prenom, string dateDeNaissance, double ? depense, statusEnum ? status, sexeEnum ? sexe) {
-            this.Nas = nas;
-            this.Nom = nom;
-            this.Prenom = prenom;
-            this.DateDeNaissance = dateDeNaissance;
-            this.Depense = depense;
-            this.Status = status;
-            this.Sexe = sexe;
-        }
-
-        public Personne(Personne p): this(p.Nom, p.Prenom, p.Depense) {
-            //constructeur de copy
-        }
-        public Personne(): this("", "", 0) {
-            //constructeur de copy
-        }
-        # endregion
+    public Personne(string nom, string prenom, int age)
+    {
+        this._nom = nom;
+        this._prenom = prenom;
+        this._age = age;
     }
 
-
+    public void SePresenter()
+    {
+        Console.WriteLine("Bonjour, je suis " + this._prenom + " " + this._nom + " et j'ai " + this._age);
+    }
 }
